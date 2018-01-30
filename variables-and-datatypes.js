00b12caf4b84adcbd9a1bfde2d47a8cb61946cc2
jefferson Griebel <jefferson.griebel@gmail.com>
2018-01-29 19:35:41 -0500

    explored the basic creation of string and numbers

--- variables-and-datatypes.js
+++ variables-and-datatypes.js
@@ -10,3 +10,28 @@ let veggie = "carrot"; //similar to var, but some other differences...
 console.log("name: ", name); //print
 console.log("fruit: ", fruit);
 console.log("veggie: ", veggie);
+
+//Simple Data Types
+//Strings
+const aString = "things and stuff";
+//a string => spaces in variable names are.... BAD AS F!!!!!
+//so we remove the space and capitalize all words but the first
+// this is called camel casing CONVENTION!!!
+const anotherString = 'some other things and stuff'; //this is single quotes
+const stringWithNumbers = '1234.56.05055';
+console.log('aString: ', aString);
+console.log('anotherString: ', anotherString);
+console.log('stringWithNumbers: ', stringWithNumbers);
+console.log('what is the type of aString? ', typeof aString);
+// Strings will come back up. There is more to know...
+// Numbers
+const aNumber = 1;
+const anotherNumber = 3.5;
+console.log('aNumber: ', aNumber);
+console.log('anotherNumber: ', anotherNumber);
+console.log('type of aNumber: ', typeof aNumber);
+console.log('type of anotherNumber: ', typeof anotherNumber);
+
+
+
+
